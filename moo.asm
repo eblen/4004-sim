436ada57b10b93f411a11aa38c52d3c3b5f23e00
John Eblen <jeblen@acm.org>
2023-01-13 22:29:13 -0500

    Moo game is now complete and working!

--- moo.asm
+++ moo.asm
@@ -93,7 +93,106 @@ RAR
 ADD #3
 XCH #3
 
-; For now, just print guess to tape
+; Print newline
+LDM #0
+WMP
+LDM #1
+WMP
+
+; ========= DEBUGGING - UNCOMMENT TO PRINT ANSWER TO TAPE ========
+
+; Print first digit
+; LDM #1
+; WMP
+; LD  #0
+; WMP
+
+; Print second digit
+; LDM #1
+; WMP
+; LD  #1
+; WMP
+
+; Print third digit
+; LDM #1
+; WMP
+; LD  #2
+; WMP
+
+; Print fourth digit
+; LDM #1
+; WMP
+; LD  #3
+; WMP
+
+; Print newline
+; LDM #0
+; WMP
+; LDM #1
+; WMP
+
+; ========= END DEBUGGING - UNCOMMENT TO PRINT ANSWER TO TAPE ========
+
+; Main Loop
+:MAIN
+
+; Read player guess (4 digits)
+; Six similar instructions for each digit
+
+; READ FIRST DIGIT
+; Write character set to tape
+LDM #1
+WMP
+
+; Wait for keyboard
+:kb_wait1
+JCN %0001 :kb_wait1
+
+RDR ; Read from keyboard
+WMP ; Write to tape
+XCH #4 ; Write first digit to R4
+
+
+; READ SECOND DIGIT
+; Write character set to tape
+LDM #1
+WMP
+
+; Wait for keyboard
+:kb_wait2
+JCN %0001 :kb_wait2
+
+RDR ; Read from keyboard
+WMP ; Write to tape
+XCH #5 ; Write second digit to R5
+
+
+; READ THIRD DIGIT
+; Write character set to tape
+LDM #1
+WMP
+
+; Wait for keyboard
+:kb_wait3
+JCN %0001 :kb_wait3
+
+RDR ; Read from keyboard
+WMP ; Write to tape
+XCH #6 ; Write third digit to R6
+
+
+; READ FOURTH DIGIT
+; Write character set to tape
+LDM #1
+WMP
+
+; Wait for keyboard
+:kb_wait4
+JCN %0001 :kb_wait4
+
+RDR ; Read from keyboard
+WMP ; Write to tape
+XCH #7 ; Write fourth digit to R7
 
 ; Print newline
 LDM #0
@@ -101,30 +200,152 @@ WMP
 LDM #1
 WMP
 
-; Print first digit
-LDM #1
-WMP
+; Clear previous counts
+CLB
+XCH #8
+CLB
+XCH #9
+CLB
+XCH #10
+
+; Compute new counts of bulls, cows, and sheep
 LD  #0
-WMP
+CLC
+SUB #4
+JCN %1100 :NO_BULL
+INC #8
+JUN :SECOND_DIGIT
 
-; Print second digit
-LDM #1
-WMP
+:NO_BULL
 LD  #1
-WMP
+CLC
+SUB #4
+JCN %0100 :COW_FOUND
 
-; Print third digit
-LDM #1
-WMP
 LD  #2
-WMP
+CLC
+SUB #4
+JCN %0100 :COW_FOUND
 
-; Print fourth digit
+LD  #3
+CLC
+SUB #4
+JCN %0100 :COW_FOUND
+INC #10
+JUN :SECOND_DIGIT
+
+:COW_FOUND
+INC #9
+
+:SECOND_DIGIT
+LD  #1
+CLC
+SUB #5
+JCN %1100 :NO_BULL_2
+INC #8
+JUN :THIRD_DIGIT
+
+:NO_BULL_2
+LD  #0
+CLC
+SUB #5
+JCN %0100 :COW_FOUND_2
+
+LD  #2
+CLC
+SUB #5
+JCN %0100 :COW_FOUND_2
+
+LD  #3
+CLC
+SUB #5
+JCN %0100 :COW_FOUND_2
+INC #10
+JUN :THIRD_DIGIT
+
+:COW_FOUND_2
+INC #9
+
+:THIRD_DIGIT
+LD  #2
+CLC
+SUB #6
+JCN %1100 :NO_BULL_3
+INC #8
+JUN :FOURTH_DIGIT
+
+:NO_BULL_3
+LD  #0
+CLC
+SUB #6
+JCN %0100 :COW_FOUND_3
+
+LD  #1
+CLC
+SUB #6
+JCN %0100 :COW_FOUND_3
+
+LD  #3
+CLC
+SUB #6
+JCN %0100 :COW_FOUND_3
+INC #10
+JUN :FOURTH_DIGIT
+
+:COW_FOUND_3
+INC #9
+
+:FOURTH_DIGIT
+LD  #3
+CLC
+SUB #7
+JCN %1100 :NO_BULL_4
+INC #8
+JUN :PRINT_BCS_RESULTS
+
+:NO_BULL_4
+LD  #0
+CLC
+SUB #7
+JCN %0100 :COW_FOUND_4
+
+LD  #1
+CLC
+SUB #7
+JCN %0100 :COW_FOUND_4
+
+LD  #2
+CLC
+SUB #7
+JCN %0100 :COW_FOUND_4
+INC #10
+JUN :PRINT_BCS_RESULTS
+
+:COW_FOUND_4
+INC #9
+
+
+; Print the results before returning to top of main loop
+:PRINT_BCS_RESULTS
 LDM #1
 WMP
-LD  #3
+LD  #8
 WMP
 
-; Halt
-:halt
-JUN :halt
+LDM #1
+WMP
+LD  #9
+WMP
+
+LDM #1
+WMP
+LD  #10
+WMP
+
+; Print newline
+LDM #0
+WMP
+LDM #1
+WMP
+
+JUN :MAIN
