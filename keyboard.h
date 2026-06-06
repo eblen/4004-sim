9c3089405c8875d8d7f9efd6e82124c99547ad8b
John Eblen <jeblen@acm.org>
2026-06-05 23:38:26 -0400

    Revamp the keyboard to use the shifter
    Keyboard now just reports what key, if any, is pressed on the column
    selected by the shifter, instead of reporting the actual key value.

--- keyboard.h
+++ keyboard.h
@@ -6,14 +6,16 @@
 #include "iodevice.h"
 #include "shifter.h"
 
-// A simple input device for debugging
+// A keyboard input device with a 4003 shifter, which mirrors the operation of
+// the Busicom keyboard.
 
 // To simulate how the Busicom keyboard works, input from the system
 // (port_output) sets the keyboard column to read using a 10-bit shifter.
+// Use "get_shifter" to access the shifter and connect it like all other devices
 // Two ports are used: port 0 is the input and port 1 is the clock.
 // See below for a further description of how we simulate shifter input.
 
-// For output to the system (port_input), we currently only use three columns:
+// The current keyboard only uses three columns:
 // 1  2  3
 // 4  5  6
 // 7  8  9
@@ -22,46 +24,30 @@
 class Keyboard : public iodevice, public testdevice
 {
     public:
-    Keyboard() :maxx(0), maxy(0), buffer{0,0}, bit_read_status(0), input_column(0),
-                num_input_nibbles(0)
+    Keyboard() :input_row(-1), input_col(-1)
     {
         shifter = std::make_shared<Shifter4003>();
     }
 
-    // The Busicom keyboard reports what key is pressed (assuming only one)
-    // for the column indicated by the shifter. To avoid timing and interface
-    // issues, we simply store the most recent key press and report it if it
-    // is on the inidicated column. We also assume only one key has been
-    // pressed at a time.
+    // The Busicom keyboard reports if the target key has been pressed as
+    // indicated by the shifter (column) and port id (row). We simply store
+    // the most recent key pressed and clear it (set to -1) when read.
     // TODO: The Busicom supports pressing multiple keys at a time. We need to
     // revisit how to support that after we get the rest of the Busicom working.
     Bit port_input(int port_id)
     {
         assert((port_id >= 0) && (port_id <= 3));
-        // Avoid by only calling when "test" returns true
-        assert(num_input_nibbles > 0);
 
         std::lock_guard<std::mutex> l(kb_buffer_mutex);
 
-        // Indicates bit was read twice for the same nibble
-        if (get_bit(bit_read_status, port_id) == 1) assert(false);
+	if (!key_was_pressed()) return 0;
 
-        bit_read_status = set_bit(bit_read_status, port_id, 1);
-
-	// Return bit in buffer only if shifter points to the correct column
 	// Note that 0 indicates the active column (not 1).
-	Bit ret_val = shifter->is_set(input_column) ? 0 : get_bit(buffer[0], port_id);
-
-        // All bits read for the current nibble. Reset for the next.
-        if (bit_read_status == 15)
-        {
-            buffer[0] = buffer[1];
-            buffer[1] = 0;
-            bit_read_status = 0;
-            num_input_nibbles--;
-        }
-
-        return ret_val;
+	if (!shifter->is_set(input_col) && input_row == port_id) {
+	    clear_pressed_key();
+	    return 1;
+	}
+	return 0;
     }
 
     // No output capability
@@ -71,49 +57,25 @@ class Keyboard : public iodevice, public testdevice
     // (User must keep track of whether nibble is high or low.)
     Bit test() {
         std::lock_guard<std::mutex> l(kb_buffer_mutex);
-        return (num_input_nibbles != 0);
+        return key_was_pressed();
     }
 
     // The attached shifter functions like every other io device, so give the
     // user full access.
-    std::shared_ptr<Shifter4003> get_shifter() {
+    std::shared_ptr<Shifter4003> get_shifter() const {
 	return shifter;
-    }
-
-    // User can input a single char at a time, which is broken into 2 nibbles.
-    void read_char(int input_event)
-    {
-        std::lock_guard<std::mutex> l(kb_buffer_mutex);
-        // A primitive device that does not store more than one byte at a time for output.
-        if (num_input_nibbles > 0) return;
-
-        // For now, we only recognize clicks of button 1
-        MEVENT event;
-        if (input_event == KEY_MOUSE && getmouse(&event) == OK && event.bstate & BUTTON1_CLICKED)
-        {
-            int input_value = get_clicked_key(event.x, event.y);
-            if (input_value == -1) return;
-
-            // Since we now input single digits, just store the value instead
-            // of an ASCII code.
-            buffer[0] = 0;
-            buffer[1] = input_value;
-            num_input_nibbles = 2;
-            input_column = key_columns[input_value];
-        }
     }
 
     void run()
     {
         // NCurses basic configuration
-        getmaxyx(stdscr, maxy, maxx);
         keypad(stdscr, TRUE);
         mmask_t ignore;
         mousemask(BUTTON1_CLICKED, &ignore);
 
         init_keyboard();
         refresh();
-        while(true) read_char(getch());
+        while(true) read_key(getch());
     }
 
     private:
@@ -121,73 +83,92 @@ class Keyboard : public iodevice, public testdevice
     std::shared_ptr<Shifter4003> shifter;
 
     // Currently we only support 0-9, space, and return
-    #define NUM_KEYS 12
-    const int SPACE_KEY = 10;
-    const int RETURN_KEY = 11;
+    static const int NUM_ROWS = 4;
+    static const int NUM_COLS = 3;
 
-    // Table to support using the shifter
-    const int key_columns[12] = {1,0,1,2,0,1,2,0,1,2,0,2};
-
-    int input_column;
-    int key_to_pixel[NUM_KEYS];
-    int maxx, maxy;
-    Nibble buffer[2];
-    Nibble bit_read_status;
-    Crumb num_input_nibbles;
+    int input_row;
+    int input_col;
+    int row_pos[NUM_ROWS];
+    int col_pos[NUM_COLS];
     std::mutex kb_buffer_mutex;
 
     // Compute key positions and print the keyboard
     void init_keyboard()
     {
-        // Compute pixel positions for each key
-        const int start_row = maxy * 2 / 3;
+	const char key_matrix[NUM_ROWS][NUM_COLS] =
+	{
+	    {'1', '2', '3'},
+	    {'4', '5', '6'},
+	    {'7', '8', '9'},
+	    {' ', '0', 'R'},
+	};
+
+        const int start_row = getmaxy(stdscr) * 2 / 3;
         const int start_col = 10;
         const int row_spaces = 3;
         const int col_spaces = 2;
 
-        // First row
-        int start_pixel = start_row*maxx;
-        key_to_pixel[1] = start_pixel + start_col;
-        key_to_pixel[2] = key_to_pixel[1] + row_spaces;
-        key_to_pixel[3] = key_to_pixel[2] + row_spaces;
-
-        // Second row
-        start_pixel += col_spaces*maxx;
-        key_to_pixel[4] = start_pixel + start_col;
-        key_to_pixel[5] = key_to_pixel[4] + row_spaces;
-        key_to_pixel[6] = key_to_pixel[5] + row_spaces;
-
-        // Third row
-        start_pixel += col_spaces*maxx;
-        key_to_pixel[7] = start_pixel + start_col;
-        key_to_pixel[8] = key_to_pixel[7] + row_spaces;
-        key_to_pixel[9] = key_to_pixel[8] + row_spaces;
-
-        // Space, zero, and return
-        start_pixel += col_spaces*maxx;
-        key_to_pixel[SPACE_KEY]  = start_pixel + start_col;
-        key_to_pixel[0]          = key_to_pixel[SPACE_KEY] + row_spaces;
-        key_to_pixel[RETURN_KEY] = key_to_pixel[0] + row_spaces;
-
-        // Print the keyboard
-        // Note that row spacing must match "row_spaces" defined above
-        // TODO: Consider how to use "row_spaces" without making the code
-        // overly complex.
-        mvprintw(start_row,                start_col, "1  2  3");
-        mvprintw(start_row +   col_spaces, start_col, "4  5  6");
-        mvprintw(start_row + 2*col_spaces, start_col, "7  8  9");
-        mvprintw(start_row + 3*col_spaces, start_col, "   0  $");
+	for (int i=0; i<NUM_ROWS; i++)
+	{
+	    row_pos[i] = start_row + i*row_spaces;
+            for (int j=0; j<NUM_COLS; j++)
+	    {
+	        col_pos[j] = start_col + j*col_spaces;
+		mvprintw(row_pos[i], col_pos[j], "%c", key_matrix[i][j]);
+	    }
+	}
     }
 
-    // Return key clicked for the given mouse coordinates
-    // Return -1 if no match found
-    int get_clicked_key(int xmouse, int ymouse)
+    // Convenience functions
+    bool key_was_pressed() const
     {
-        int clicked_pixel = ymouse*maxx + xmouse;
-        for (int i=0; i<NUM_KEYS; i++)
+        return (input_row > -1) && (input_col > -1);
+    }
+
+    void clear_pressed_key()
+    {
+        input_row = -1;
+        input_col = -1;
+    }
+
+    void read_key(int input_event)
+    {
+        std::lock_guard<std::mutex> l(kb_buffer_mutex);
+
+        // For now, we only recognize clicks of button 1
+        MEVENT event;
+        if (input_event == KEY_MOUSE && getmouse(&event) == OK && event.bstate & BUTTON1_CLICKED)
         {
-            if (key_to_pixel[i] == clicked_pixel) return i;
+            set_clicked_key(event.x, event.y);
         }
-        return -1;
+    }
+
+
+    // Set the clicked row and column. Set to -1 if click is not on a
+    // specific row or column.
+    void set_clicked_key(int xmouse, int ymouse)
+    {
+	// Do not overwrite any current, stored key press
+	if (key_was_pressed()) return;
+
+	input_row = -1;
+	for (int i=0; i<NUM_ROWS; i++)
+	{
+	    if (ymouse == row_pos[i])
+	    {
+	       input_row = i;
+	       break;
+	    }
+	}
+
+	input_col = -1;
+	for (int i=0; i<NUM_COLS; i++)
+	{
+	    if (xmouse == col_pos[i])
+	    {
+	       input_col = i;
+	       break;
+	    }
+	}
     }
 };
